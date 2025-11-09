21958f65ab954d01bbd794367b20cd625e1f262f
Daniil Kucherenko <daniil.kucherenko@outlook.com>
2025-11-09 22:00:35 +0100

    fix: change parser for ALLOWED_ORIGINS in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,31 +1,34 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+from pydantic import Field, field_validator
 from typing import List
 from functools import lru_cache
-
-# DEBUG
-
-# import pathlib
-# DOTENV = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), ".env")
-# print("DOTENV PATH:", DOTENV)
-# print("EXISTS:", pathlib.Path(DOTENV).exists())
-
+import json
 
 class Settings(BaseSettings):
     SECRET_KEY: str = Field(...)
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-
     RATE_LIMIT_PER_MINUTE: int = 60
-
     MODEL_PATH: str = "model.joblib"
-
     DATABASE_URL: str = "sqlite:///./data/users.db"
 
     ALLOWED_ORIGINS: List[str] = [
         "http://localhost:3000",
         "http://localhost:8000",
     ]
+
+    @field_validator("ALLOWED_ORIGINS", mode="before")
+    def parse_allowed_origins(cls, v):
+        if isinstance(v, str):
+            v = v.strip()
+            if v.startswith("[") and v.endswith("]"):
+                try:
+                    return json.loads(v)
+                except json.JSONDecodeError:
+                    pass
+
+            return [x.strip().strip('"').strip("'") for x in v.split(",") if x.strip()]
+        return v
 
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -40,5 +43,4 @@ def get_settings() -> Settings:
 settings = get_settings()
 
 if __name__ == "__main__":
-    settings = get_settings()
-    print(settings.SECRET_KEY)
+    print(settings.ALLOWED_ORIGINS)
